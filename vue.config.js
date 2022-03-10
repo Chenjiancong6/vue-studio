eeb35f72559ad7868a11940d1c042f85ed935d6a
chenjiancong <1293707549@qq.com>
2022-03-11 00:32:13 +0800

    feat: vue-studio项目第一次提交到github远程仓库，该项目用于个人学习和成长，并构建属于个人的公共组件库

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // 1.配置方式一: CLI提供的属性
     outputDir: "./build",
     publicPath: "./",
-    // 2.配置方式二: 和webpack属性完全一致, 最后会进行合并
+    // 2.配置方式二: 和webpack属性完全一致, 最后会进行合
     configureWebpack: {
         resolve: {
             alias: {

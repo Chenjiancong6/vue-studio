939076a97b3def8af78a8c966eadea7779a5322b
chenjiancong <1293707549@qq.com>
2022-03-12 13:36:34 +0800

    element-plus组件库引用

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { createApp } from "vue"
-import App from "./App.vue"
-import router from "./router"
-import store from "./store"
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementPlus from 'element-plus'; // 全局引入
+import 'element-plus/dist/index.css'; // element样式
 
-createApp(App).use(store).use(router).mount("#app")
+
+//  这里注册全局的组件和库
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(ElementPlus);
+app.mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
         <h1>{{ $store.state.name }}</h1>
+        <el-button type="primary" plain>Primary</el-button>
+        <el-button type="success" plain>Success</el-button>
+        <el-button type="danger">Info</el-button>
     </div>
 </template>
 

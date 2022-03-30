1dd4c4777a23d8f6c8e7dacd758114cb16dbfa61
chenjiancong <1293707549@qq.com>
2022-03-30 22:46:40 +0800

    feat: 首页布局

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,15 +1,85 @@
 <template>
-  <div>Main</div>
+  <div class="main">
+    <el-container class="main-content">
+        <el-aside width="200px">Aside</el-aside>
+        <el-container class="page">
+            <el-header class="page-header">
+                <div>Header</div>
+            </el-header>
+            <el-main class="page-content">
+                <div class="page-info">
+                    Main
+                </div>
+            </el-main>
+        </el-container>
+    </el-container>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  setup() {
-    return {}
-  }
-})
+    setup() {
+        return {};
+    }
+});
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.main {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+.main-content,
+.page {
+    height: 100%;
+}
+
+.page-content {
+    height: calc(100% - 48px);
+    // .page-info {
+    //     background-color: #fff;
+    //     border-radius: 5px;
+    // }
+}
+
+.el-header,
+.el-footer {
+    display: flex;
+    color: #333;
+    text-align: center;
+    align-items: center;
+}
+
+.el-header {
+    height: 48px !important;
+    background-color: #f0f2f5;
+}
+
+.el-aside {
+    overflow-x: hidden;
+    overflow-y: auto;
+    line-height: 200px;
+    text-align: left;
+    cursor: pointer;
+    background-color: #e6e6e6;
+    transition: width 0.3s linear;
+    scrollbar-width: none; /* firefox */
+    -ms-overflow-style: none; /* IE 10+ */
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
+}
+
+.el-main {
+    color: #333;
+    text-align: center;
+    // background-color: #f0f2f5;
+}
+</style>

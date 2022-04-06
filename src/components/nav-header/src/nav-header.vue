1fa2920714ec323da1d7e5538dc31f24b6f71b20
chenjiancong <1293707549@qq.com>
2022-04-06 22:52:54 +0800

    feat:创建顶部导航栏用户信息

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -4,25 +4,30 @@
             <el-icon v-if="!isFold"><d-arrow-right /></el-icon>
             <el-icon v-if="isFold"><d-arrow-left /></el-icon>
         </div>
+        <div class="content">
+            <user-info />
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-import { DArrowRight, DArrowLeft } from "@element-plus/icons-vue";
+import { defineComponent, ref } from 'vue';
+import { DArrowRight, DArrowLeft } from '@element-plus/icons-vue';
+import UserInfo from './user-info.vue';
 export default defineComponent({
     // vue3，子组件向父组件传值的方法
-    emits: ["foldChange"],
+    emits: ['foldChange'],
     components: {
         DArrowRight,
-        DArrowLeft
+        DArrowLeft,
+        UserInfo
     },
     setup(props, { emit }) {
         const isFold = ref(false);
         const handleFoldClick = () => {
             isFold.value = !isFold.value; // 取反
             // 子组件向父组件传值
-            emit("foldChange", isFold.value);
+            emit('foldChange', isFold.value);
         };
         return {
             isFold,
@@ -34,9 +39,18 @@ export default defineComponent({
 
 <style scope lang="less">
 .nav-header {
+    display: flex;
+    width: 100%;
     .fold-menu {
         // font-size: 30px;
         cursor: pointer;
+    }
+    .content {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        flex: 1;
+        padding: 0 20px;
     }
 }
 </style>
